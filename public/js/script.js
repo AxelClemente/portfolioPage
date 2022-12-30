c34640605cbbf2c913324fb8ddfe9133ab11382b
Axel <axel_clemente@yahoo.com>
2022-12-30 11:02:33 +0100

    Particles working

--- public/js/script.js
+++ public/js/script.js
@@ -17,15 +17,15 @@ window.addEventListener('load', function(){
     constructor(effect, x, y, color){
       this.effect = effect;
       this.x = Math.random()* this.effect.canvasWidth;
-      this.y = 0;
+      this.y = this.effect.canvasHeigth; // <------------ CHANGE THE ORIGINAL STARTING POINT OF PARTICLES
       this.color = color;
       this.originX = x;
-      this.originY = y;
-      this.size = this.effect.gap; // <------------ TO CHECK
+      this.originY = y; 
+      this.size = this.effect.gap ; // <------------ TO CHECK
       this.dx = 0;  
       this.dy = 0;
       this.vx = 0;
-      this.dy = 0;
+      this.vy = 0;
       this.force = 0;
       this.angle = 0;
       this.distance = 0;
@@ -35,10 +35,22 @@ window.addEventListener('load', function(){
     }
     draw(){
       this.effect.context.fillStyle = this.color; 
-      this.effect.context.fillRect(this.originX, this.originY, this.size, this.size); 
+      this.effect.context.fillRect(this.x, this.y, this.size, this.size); 
     }
     update(){
-      this.x += this.originX - this.x;
+      this.dx = this.effect.mouse.x - this.x;
+      this.dy = this.effect.mouse.y - this.y;
+      this.distance = (this.dx * this.dx + this.dy * this.dy);
+      this.force = -this.effect.mouse.radius / this.distance;
+
+      if (this.distance < this.effect.mouse.radius){
+        this.angle = Math.atan2(this.dy, this.dx);
+        this.vx += this.force * Math.cos(this.angle);
+        this.vy += this.force * Math.sin(this.angle)
+      }
+
+      this.x += (this.vx *= this.friction) + (this.originX - this.x) * this.ease;
+      this.y += (this.vy *= this.friction) + (this.originY - this.y) * this.ease;
     }
   }
 
@@ -141,6 +153,7 @@ window.addEventListener('load', function(){
   effect.render();
  
   function animate(){
+    ctx.clearRect(0, 0, canvas.width,canvas.height)
     effect.render();
     requestAnimationFrame(animate);
     
