7bfe84ddfe058922bc87f3e6b95045c08f84a395
Axel <axel_clemente@yahoo.com>
2023-01-07 18:10:18 +0100

    To Check

--- public/js/script.js
+++ public/js/script.js
@@ -444,3 +444,17 @@ modalCloses = document.querySelectorAll('.services__modal-close')
 let modal = function(modalClick){
   modalViews[modalClick].classList.add('active-modal')
 }
+
+modalBtns.forEach((modalBtn, i)=>{
+  modalBtn.addEventListener('click', ()=>{
+    modal(i)
+  })
+})
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener('click', () =>{
+    modalViews.forEach((modalView) =>{
+      modalView.classList.remove('active-modal')
+    })
+  } )
+})
